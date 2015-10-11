20ebfc831d152a2453175361b3c59c1fc711ae3f
LucieCBurgess <lucie.burgess@icloud.com>
2015-10-11 21:28:47 +0100

    Revert "Added a bug! Not sure where. Need to revert to previous commit"
    This reverts commit 4853290fb24b3a8b8e074b4e0444d38d3c488391.
    
    Reversing last commit due to introduced bug

--- X011_PokerHandsv2.groovy
+++ X011_PokerHandsv2.groovy
@@ -15,7 +15,7 @@ println "Enter C for clubs, S for spades, H for hearts, D for diamonds"
 println "Then, pick the number of your card: it can be 1,2,3,4,5,6,7,8,9,10,J,Q,K"
 println "For example, enter C1 which is the ace of clubs, or DK which is the King of diamonds"
 
-String cardA, cardB, cardC, cardD, cardE
+String card1, card2, card3, card4, card5
 
 int i = 0
 int min = 0 
@@ -23,8 +23,8 @@ int minmed = 0
 int middle = 0
 int maxmed = 0 
 int max = 0 
-int numrank, numrankA, numrankB, numrankC, numrankD, numrankE
-String rankA, suitA, rankB, suitB, rankC, suitC, rankD, suitD, rankE, suitD
+int numrank, numrank1, numrank2, numrank3, numrank4, numrank5
+String rank1, suit1, rank2, suit2, rank3, suit3, rank4, suit4, rank5, suit5
  
 while (i < 5) {
 i++;
@@ -56,65 +56,58 @@ println suit + " " + rank
 println numrank		
 		
 	if (i == 1) {							//for i = 1, assign values to numrank, rank and suit from 1st card
-		numrankA = numrank
-		rankA = rank
-		suitA = suit
-		println "Rank value is" + " " + numrankA
-		println "The card you entered was " + suitA + rankA
+		numrank1 = numrank
+		rank1 = rank
+		suit1 = suit
+		println "Rank value is" + " " + numrank1
+		println "The card you entered was " + suit1 + rank1
 	}	// End of loop i=1
 									
 		
 	if (i == 2) {
-		numrankB = numrank
-		rankB = rank
-		suitB = suit
-		println "Rank value is" + " " + numrankB
-		println "The card you entered was " + suitB + rankB
+		numrank2 = numrank
+		rank2 = rank
+		suit2 = suit
+		println "Rank value is" + " " + numrank2
+		println "The card you entered was " + suit2 + rank2
 
-		if (numrankB > numrankA) {
-			max = numrankB
-			min = numrankA
+		if (numrank2 > numrank1) {
+			max = numrank2
+			min = numrank1
 		} else { 
-			max = numrankA
-			min = numrankB 
+			min = numrank2
+			max = numrank1 
 		}
 		println "Cards sorted by rank are " + min + " " + max
 	}									//End of loop i=2
 	
 	if (i == 3) {
-		numrankC = numrank
-		rankC = rank
-		suitC = suit
-		println "Rank value is" + " " + numrankC
-		println "The card you entered was " + suitC + rankC
+		numrank3 = numrank
+		rank3 = rank
+		suit3 = suit
+		println "Rank value is" + " " + numrank3
+		println "The card you entered was " + suit3 + rank3
 	
 	
-		if (numrankC >= max) {
+		if (numrank3 >= max) {
 			middle = max
-			max = numrankC
-		} else if ((numrankC <= max) && (numrankC >= min)) {
-			middle = numrankC
-		} else if (numrankC <= min) {
+			max = numrank3
+		} else if ((numrank3 <= max) && (numrank3 >= min)) {
+			middle = numrank3
+		} else if (numrank3 <= min) {
 			middle = min
-			min = numrankC
+			min = numrank3
 		}
 		println "Cards sorted by rank are " + min + " " + middle + " " + max 	// min, middle, max
-		if (max == numrankA) 
-				
-		
 	} // End of loop 3
 	
 	if (i == 4) {
-		numrankD = numrank
-		rankD = rank
-		suitD = suit
-		println "Rank value is" + " " + numrankC
-		println "The card you entered was " + suitC + rankC
-	
-
+		numrank4 = numrank
+		rank4 = rank
+		suit4 = suit
+		
 		if (numrank4 >= max) {
-			middle = max
-			max = numrank4
+			numrank4 = max
 		} else if ((numrank4 <= max) && (numrank4 >= middle)) {
 			numrank4 = maxmed
 		} else if (numrank4 == middle) {
